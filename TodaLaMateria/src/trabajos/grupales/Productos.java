600da1a83a19588279e67aa30e784efc819f695c
AndrosBY <AndrosBY@AndrosPC-13>
2021-08-13 12:29:38 -0500

    robot girls z

---
+++ TodaLaMateria/src/trabajos/grupales/Productos.java
@@ -0,0 +1,22 @@
+package trabajos.grupales;
+
+public class Productos {
+	
+	public void seleccion(int producto) {
+
+		if (producto == 1) {
+			System.out.println("Precio: 3$");
+		} else if (producto == 2) {
+			System.out.println("Precio: 10$");
+		} else if (producto == 3) {
+			System.out.println("Precio: 2$");
+		} else {
+			System.out.println("El producto no existe");
+		}
+
+	}
+	public void MetodoPago(String MetodoPago) {
+		// TODO Auto-generated method stub
+		System.out.println("Metodo de pago: " + MetodoPago);
+	}
+}
